39d6dcb8c92851ef161896d90b17b3891037d9f4
Lawson Kelly <wlawsonkelly@gmail.com>
2020-12-01 14:09:41 -0500

    Pushing backend

--- server.js
+++ server.js
@@ -1,89 +1,77 @@
 var express = require("express");
 var path = require("path");
 
-// Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = 3000;
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Star Wars Characters (DATA)
-// =============================================================
 var tables = [
   {
-    routeName: "yoda",
     name: "Yoda",
-    email: "Jedi Master",
+    email: "yoda@gmial.com",
     phone: 900,
     id: 2000
   },
   {
-    routeName: "yoda",
-    name: "Yoda",
-    email: "Jedi Master",
+    name: "obi",
+    email: "obi@gmail.com",
     phone: 900,
     id: 2000
   },
   {
-    routeName: "yoda",
-    name: "Yoda",
-    email: "Jedi Master",
-    phone: 900,
+    name: "darth",
+    email: "darth@gmail.com",
+    phone: 1800,
     id: 2000
   }
-  
 ];
-// Routes
-// =============================================================
 
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
-});
-
-app.get("/add", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
-});
-
-// Displays all characters
-app.get("/api/characters", function(req, res) {
-  return res.json(characters);
-});
-
-// Displays a single character, or returns false
-app.get("/api/characters/:character", function(req, res) {
-  var chosen = req.params.character;
-
-  console.log(chosen);
-
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
+var waitList = [
+    {
+        
     }
+]
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+
+app.get("/api/tables", function(req, res) {
+  return res.json(tables);
+});
+
+app.get("/api/waitlist", function(req, res) {
+    return res.json(tables);
+  });
+
+app.post("/api/tables", function(req, res) {
+  var newTable = req.body;
+  console.log(newTable);
+
+  if (tables.length < 5) {
+    tables.push(newTable);
+    //res.json(newTable);
+  } else {
+    waitList.push(newTable);
+   //res.json(newTable);
   }
 
-  return res.json(false);
 });
 
-// Create New Characters - takes in JSON input
-app.post("/api/characters", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newCharacter = req.body;
-
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newCharacter);
-
-  characters.push(newCharacter);
-
-  res.json(newCharacter);
-});
+app.post("/api/tables", function(req, res) {
+  tables = [];
+  waitList = [];
+  });
 
 // Starts the server to begin listening
 // =============================================================
