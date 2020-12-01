95622f9ec6889dc039506f8a1ef303931c140162
tylerdahl123 <dahlgren15@gmail.com>
2020-12-01 12:18:06 -0800

    trying to get heroku working

--- server.js
+++ server.js
@@ -32,13 +32,13 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
 });
 
-app.get("/tables", function(req, res) {
-  res.sendFile(path.join(__dirname, "tables.html"));
-});
+// app.get("/tables", function(req, res) {
+//   res.sendFile(path.join(__dirname, "tables.html"));
+// });
 
-app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "reserve.html"));
-  });
+// app.get("/reserve", function(req, res) {
+//     res.sendFile(path.join(__dirname, "reserve.html"));
+//   });
 
 app.get("/api/tables", function(req, res) {
   return res.json(tables);
